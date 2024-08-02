c6ce83fa37ce0fc188699eda33b7fac134202ef1
MykhailoVobolis <mvobolis@gmail.com>
2024-08-02 11:09:11 +0300

    init commit

--- src/server.js
+++ src/server.js
@@ -9,34 +9,35 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { swaggerDocs } from './middlewares/swaggerDocs.js';
 import Routers from './routers/index.js';
 
-const PORT =  Number(process.env.PORT) ? Number(process.env.PORT) : 3000;
+const PORT = Number(process.env.PORT) ? Number(process.env.PORT) : 3000;
 
 export const setupServer = () => {
+  const app = express();
 
-    const app = express();
+  app.use(cors());
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+    }),
+  );
+  app.use(cookieParser());
+  app.use('/api-docs', swaggerDocs());
 
-    app.use(cors());
-    app.use(express.json({
-        type: ['application/json', 'application/vnd.api+json'],
-    }));
-    app.use(cookieParser());
-    app.use('/api-docs', swaggerDocs());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-    app.use(
-        pino({
-        transport: {
-            target: 'pino-pretty',
-        },
-        }),
-    );
+  app.use(Routers);
 
-    app.use(Routers);
+  app.use('*', notFoundHandler);
 
-    app.use('*', notFoundHandler);
+  app.use(errorHandler);
 
-    app.use(errorHandler);
-
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 };
