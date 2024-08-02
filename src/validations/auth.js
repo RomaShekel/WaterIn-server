4f9f8851883c68ab3972fa7c0dcf5c2d50073db1
RomaShekel <141637507+RomaShekel@users.noreply.github.com>
2024-08-02 19:41:07 +0300

    Update auth.js
    deleted a name field couse it's dosn't use in users registration

--- src/validations/auth.js
+++ src/validations/auth.js
@@ -2,7 +2,6 @@ import Joi from 'joi';
 import { emailRegexp } from '../constants/user.js';
 
 export const registerUserSchema = Joi.object({
-  // name: Joi.string().min(3).max(30).required(), імені при реєстрації у нас немає потрібно прибрати
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
 }).messages({
