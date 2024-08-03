422b7af39b935b0b13010495603fe271dd896ce7
MykhailoVobolis <mvobolis@gmail.com>
2024-08-03 21:25:57 +0300

    add login swagger

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,14 +1,13 @@
 // import authRouter from './auth.js';
 import waterRouter from './water.js';
 import usersRouter from './users.js';
-import authRouter from './auth.js'
+import authRouter from './auth.js';
 import { Router } from 'express';
 
 const router = Router();
 
-router.use('/users', authRouter);
+router.use('/auth', authRouter);
 router.use('/water', waterRouter);
 router.use('/users', usersRouter);
-
 
 export default router;
