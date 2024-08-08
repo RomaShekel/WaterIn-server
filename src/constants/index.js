b153d58aa8eaf56186674bd2ffca3f0c46078c11
RomaShekel <141637507+RomaShekel@users.noreply.github.com>
2024-08-08 23:19:42 +0300

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,7 @@ export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
 };
-export const FIFTEEN_MINUTES = 60 * 1000;
+export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
 
 export const SMTP = {
