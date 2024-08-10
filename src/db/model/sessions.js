9920c593e7bd44245e069e03a5d850b9c72350a7
Shekel Roman <romansekel37@gmail.com>
2024-08-10 21:16:21 +0300

    1

--- src/db/model/sessions.js
+++ src/db/model/sessions.js
@@ -2,7 +2,7 @@ import { model, Schema } from 'mongoose';
 
 const sessionSchema = new Schema(
   {
-    userId: { type: Schema.Types.ObjectId, ref: 'users' },
+    userId: { type: String, ref: 'users' },
     accessToken: { type: String, required: true },
     refreshToken: { type: String, required: true },
     accessTokenValidUntil: { type: Date, required: true },
--- src/db/model/water.js
+++ src/db/model/water.js
@@ -1,7 +1,7 @@
 import { model, Schema } from 'mongoose';
 const waterNoteSchema = new Schema(
   {
-    userId: {type: Schema.Types.ObjectId, ref: 'users' },
+    userId: {type: String, ref: 'users' },
     volume: { type: Number, required: true },
     drinkTime: { type: String, required: true },
     createdAt: { type: Number, default: () => Date.now()},
